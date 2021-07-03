00df9b1a4b1a1b8741ac898b323ffc6982200b9e
rathu <rathushan.20191029@iit.ac.lk>
2021-07-03 09:02:28 +0530

    navbar changed

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,34 +1,19 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import NavLink from './NavLink';
 import './Header.css';
 
 
 class Header extends React.Component {
-    constructor(props){
-        super(props);
-        this.state={
-            className: '',
-        }
-    }
-
-    onClick(){
-        console.log("cliekd")
-        const pathname = window.location.pathname;
-        console.log(pathname);
-        var isActive = pathname === this.props.to;
-        this.setState({className: isActive ? 'active' : ''});
-        console.log(pathname + " " + this.props.to);
-    }
 
     render () {
 
         return(
             <nav className="navibar">
-            <ul onClick={() => this.onClick()}>
-                <li className={this.state.className} to="/"><NavLink to="/">Albums</NavLink></li>
-                <li className={this.state.className} to="/photos"><NavLink to="/photos">Photos</NavLink></li>
+            <ul>
+                <li><NavLink className="maio" to="/">Albums</NavLink></li>
+                <li><NavLink className="maio" to="/photos">Photos</NavLink></li>
             </ul> 
-        </nav>
+            </nav>
         );
     }
 
--- src/components/header/NavLink.js
+++ src/components/header/NavLink.js
@@ -2,19 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
+import './Header.css';
 
 
 class NavLink extends React.Component {
 
     render() {
-        const { pathname } = this.props.location;
+        
+        var pathname  = this.props.location.pathname;
         var isActive = pathname === this.props.to;
         var className = isActive ? 'active' : '';
 
         return(
-            <Link className={className} {...this.props}>
+            <div className={className}>
+                <Link {...this.props}>
                 {this.props.children}
             </Link>
+            </div>
+            
         );
     }
 }
